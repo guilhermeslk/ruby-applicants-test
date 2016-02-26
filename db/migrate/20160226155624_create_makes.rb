1f506b8586eecc24af1c0834d6e2ed5b216438b1
Marcelo Munhoz Pélos <mpelos@gmail.com>
2016-02-26 15:50:24 -0300

    Implementa funcionalidades do sistema.

--- db/migrate/20160226155624_create_makes.rb
+++ db/migrate/20160226155624_create_makes.rb
@@ -2,6 +2,7 @@ class CreateMakes < ActiveRecord::Migration
   def change
     create_table :makes do |t|
       t.string :name
+      t.integer :webmotors_id
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226155624) do
+ActiveRecord::Schema.define(version: 20160226182941) do
 
   create_table "makes", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "webmotors_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "models", force: :cascade do |t|
+    t.integer  "make_id"
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
