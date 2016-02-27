4756299f7cc2440646df1d3288694e88810f0840
Guilherme Solinscki <guilherme.slnk@gmail.com>
2016-02-27 11:26:16 -0300

    Extrai lógica do controller para o model

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,2 +1,17 @@
 class Model < ActiveRecord::Base
+  def self.get_uri
+    URI("#{ENV['BASE_URL']}/carro/modelos")
+  end
+
+  def self.request_all(make_id)
+    response = Net::HTTP.post_form(get_uri, { marca: make_id })
+    JSON.parse response.body
+  end
+
+  def self.fetch_and_save(make_id)
+    make = Make.where(webmotors_id: make_id)[0]
+    request_all(make_id).each do |params|
+      create(make_id: make.id, name: params["Nome"]) if where(name: params["Nome"], make_id: make.id).size == 0
+    end
+  end
 end
