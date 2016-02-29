c77f9d19db5259d1cc15502636620877def7bc63
Guilherme Solinscki <guilherme.slnk@gmail.com>
2016-02-29 16:01:17 -0300

    Extrai lógica de acesso a API para módulo

---
+++ lib/web_motors_api_service.rb
@@ -0,0 +1,18 @@
+module WebMotorsApiService
+  def self.included(base)
+    base.send :extend, ClassMethods
+  end
+
+  module ClassMethods
+    attr_accessor :uri
+
+    def base_uri(uri)
+      self.uri = URI("#{ENV['BASE_URL']}#{uri}")
+    end
+
+    def request_all(params = {})
+      response = Net::HTTP.post_form(self.uri, params)
+      JSON.parse response.body
+    end
+  end
+end
