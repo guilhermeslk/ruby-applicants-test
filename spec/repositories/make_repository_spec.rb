eb60eb19ffddb04edfbaec37d8246e42a8db27d2
Guilherme Solinscki <guilherme.slnk@gmail.com>
2016-02-29 23:32:36 -0300

    Adiciona teste de divergência de dados locais

--- spec/repositories/make_repository_spec.rb
+++ spec/repositories/make_repository_spec.rb
@@ -3,20 +3,26 @@ require 'rails_helper'
 describe 'MakeRepository' do
   describe 'fetch_and_save' do
     let(:response) do
-      VCR.use_cassette('makes')  do
-        $response = Net::HTTP.post_form(URI('http://www.webmotors.com.br/carro/marcas'), {})
-      end
+      VCR.use_cassette('makes')  { $response = Net::HTTP.post_form(URI('http://www.webmotors.com.br/carro/marcas'), {}) }
+    end
+
+    before(:each) do
+      VCR.use_cassette('makes')  { MakeRepository.fetch_and_save }
     end
 
     context 'when there is no local data' do
-      before(:each) do
-        VCR.use_cassette('makes')  do
-          MakeRepository.fetch_and_save
-        end
-      end
-
       it 'fetches data from the api and records it into the database' do
         expect(Make.all.count).to eq(JSON.parse(response.body).size)
+      end
+    end
+
+    context 'when there is local data' do
+      it 'fetches data from the api and records only new data' do
+        make = Make.last
+        name = make.name
+        make.destroy
+        VCR.use_cassette('makes') { MakeRepository.fetch_and_save }
+        expect(Make.find_by_name(name)).not_to be_nil
       end
     end
   end
